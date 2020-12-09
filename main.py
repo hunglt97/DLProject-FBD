7350d1a2bb95c973380a1d5fc7d03b58aeb9608d
hunglt97 <33819290+hunglt97@users.noreply.github.com>
2020-12-10 00:08:12 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,123 +1,54 @@
-# This is a sample Python script.
+import tensorflow.keras as keras
+from sklearn.metrics import accuracy_score
+import numpy as np
+import model as fbdnet
+import utils1
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-class PlotProgress(keras.callbacks.Callback):  # Plot graph after each epoch
-
-    def __init__(self, entity='loss'):
-        self.entity = entity
-
-    def on_train_begin(self, logs={}):
-        self.i = 0
-        self.x = []
-        self.losses = []
-        self.val_losses = []
-
-        self.fig = plt.figure()
-
-        self.logs = []
-
-    def on_epoch_end(self, epoch, logs={}):
-        self.logs.append(logs)
-        self.x.append(self.i)
-        self.losses.append(logs.get('{}'.format(self.entity)))
-        self.val_losses.append(logs.get('val_{}'.format(self.entity)))
-        self.i += 1
-
-        clear_output(wait=True)
-        plt.plot(self.x, self.losses, label="{}".format(self.entity))
-        plt.plot(self.x, self.val_losses, label="val_{}".format(self.entity))
-        plt.legend()
-        plt.show()
-
-
-
-# Input
-inputs = Input(shape=x_train[0].shape, name='main_input')
-
-
-# load main-branch model
-# main_branch = keras.models.load_model('path/to/location')(inputs)
-def main_vgg():
-    # the 1-st block
-    conv1_1 = Conv2D(64, kernel_size=3, activation='relu', padding='same', name='conv1_1')
-    conv1_1 = BatchNormalization()(conv1_1)
-    conv1_2 = Conv2D(64, kernel_size=3, activation='relu', padding='same', name='conv1_2')(conv1_1)
-    conv1_2 = BatchNormalization()(conv1_2)
-    pool1_1 = MaxPooling2D(pool_size=(2, 2), name='pool1_1')(conv1_2)
-    drop1_1 = Dropout(0.3, name='drop1_1')(pool1_1)
-
-    # the 2-nd block
-    conv2_1 = Conv2D(128, kernel_size=3, activation='relu', padding='same', name='conv2_1')(drop1_1)
-    conv2_1 = BatchNormalization()(conv2_1)
-    conv2_2 = Conv2D(128, kernel_size=3, activation='relu', padding='same', name='conv2_2')(conv2_1)
-    conv2_2 = BatchNormalization()(conv2_2)
-    conv2_3 = Conv2D(128, kernel_size=3, activation='relu', padding='same', name='conv2_3')(conv2_2)
-    conv2_3 = BatchNormalization()(conv2_3)
-    pool2_1 = MaxPooling2D(pool_size=(2, 2), name='pool2_1')(conv2_3)
-    drop2_1 = Dropout(0.3, name='drop2_1')(pool2_1)
-
-    # the 3-rd block
-    conv3_1 = Conv2D(256, kernel_size=3, activation='relu', padding='same', name='conv3_1')(drop2_1)
-    conv3_1 = BatchNormalization()(conv3_1)
-    conv3_2 = Conv2D(256, kernel_size=3, activation='relu', padding='same', name='conv3_2')(conv3_1)
-    conv3_2 = BatchNormalization()(conv3_2)
-    conv3_3 = Conv2D(256, kernel_size=3, activation='relu', padding='same', name='conv3_3')(conv3_2)
-    conv3_3 = BatchNormalization()(conv3_3)
-    conv3_4 = Conv2D(256, kernel_size=3, activation='relu', padding='same', name='conv3_4')(conv3_3)
-    conv3_4 = BatchNormalization()(conv3_4)
-    pool3_1 = MaxPooling2D(pool_size=(2, 2), name='pool3_1')(conv3_4)
-    drop3_1 = Dropout(0.3, name='drop3_1')(pool3_1)
-
-    # the 4-th block
-    conv4_1 = Conv2D(256, kernel_size=3, activation='relu', padding='same', name='conv4_1')(drop3_1)
-    conv4_1 = BatchNormalization()(conv4_1)
-    conv4_2 = Conv2D(256, kernel_size=3, activation='relu', padding='same', name='conv4_2')(conv4_1)
-    conv4_2 = BatchNormalization()(conv4_2)
-    conv4_3 = Conv2D(256, kernel_size=3, activation='relu', padding='same', name='conv4_3')(conv4_2)
-    conv4_3 = BatchNormalization()(conv4_3)
-    conv4_4 = Conv2D(256, kernel_size=3, activation='relu', padding='same', name='conv4_4')(conv4_3)
-    conv4_4 = BatchNormalization()(conv4_4)
-    pool4_1 = MaxPooling2D(pool_size=(2, 2), name='pool4_1')(conv4_4)
-    drop4_1 = Dropout(0.3, name='drop4_1')(pool4_1)
-
-    # the 5-th block
-    conv5_1 = Conv2D(512, kernel_size=3, activation='relu', padding='same', name='conv5_1')(drop4_1)
-    conv5_1 = BatchNormalization()(conv5_1)
-    conv5_2 = Conv2D(512, kernel_size=3, activation='relu', padding='same', name='conv5_2')(conv5_1)
-    conv5_2 = BatchNormalization()(conv5_2)
-    conv5_3 = Conv2D(512, kernel_size=3, activation='relu', padding='same', name='conv5_3')(conv5_2)
-    conv5_3 = BatchNormalization()(conv5_3)
-    conv5_4 = Conv2D(512, kernel_size=3, activation='relu', padding='same', name='conv5_4')(conv5_3)
-    conv5_4 = BatchNormalization()(conv5_4)
-    pool5_1 = MaxPooling2D(pool_size=(2, 2), name='pool5_1')(conv5_4)
-    drop5_1 = Dropout(0.3, name='drop5_1')(pool5_1)
-
-    # Flatten and output
-    flatten = Flatten(name='flatten')(drop5_1)
-
-    # create model
-    main_branch_vgg = Model(inputs=conv1_1, outputs=flatten)
-
-    return main_branch_vgg
-
-plot_progress = PlotProgress(entity='loss')
-
-try:
-
-except KeyboardInterrupt:
-    pass
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    # import data
+    (x_train, y_train), (x_test, y_test) = utils1.get_data('SCUT-FBP5500_v2/')
+    y_train_beauty = y_train[:, 0]
+    y_train_race = y_train[:, 1]
+    y_train_gender = y_train[:, 2]
+    y_test_beauty = y_test[:, 0]
+    y_test_race = y_test[:, 1]
+    y_test_gender = y_test[:, 2]
+
+    x_train = x_train.reshape(3300, 224, 224, 3)
+    x_test = x_test.reshape(2200, 224, 224, 3)
+    x_train = keras.applications.resnet50.preprocess_input(x_train)
+    x_test = keras.applications.resnet50.preprocess_input(x_test)
+
+    filepath = "weights.hdf5"
+    # model = fbdnet.my_model()
+    # fbdnet.train_model(x_train, y_train_beauty, y_train_race, y_train_gender, filepath)
+    #
+    # model.load_weights('weights_min_loss.hdf5')
+    # y_pred = model.predict(x_test)
+    # y_pred_beauty = np.squeeze(y_pred[0])
+
+    y_pred_beauty, y_pred_race, y_pred_gender = fbdnet.predict_model(x_test, filepath)
+
+    cov = np.cov(y_pred_beauty, y_test_beauty)
+    print('pc =', cov/(np.std(y_pred_beauty)*np.std(y_test_beauty)))
+    print('mae =', np.mean(np.abs(y_pred_beauty-y_test_beauty)))
+    print('rmse =', np.sqrt(np.mean(np.square(y_pred_beauty-y_test_beauty))))
+    print('race acc', accuracy_score(y_test_race, y_pred_race))
+    print('gender acc', accuracy_score(y_test_gender, y_pred_gender))
+
+    test_img = x_test[1]
+    test_img = test_img[np.newaxis, :, :, :]
+    y_pred_beauty_1, y_pred_race_1, y_pred_gender_1 = fbdnet.predict_model(test_img, filepath)
+    print(y_test_beauty[1], y_pred_beauty_1)
+    print(y_test_race[1], y_pred_race_1)
+    print(y_test_gender[1], y_pred_gender_1)
+
+    image = keras.preprocessing.image.load_img('test1.jpg', target_size=(224, 224))
+    input_arr = keras.preprocessing.image.img_to_array(image)
+    input_arr = np.array([input_arr])
+    print(fbdnet.predict_model(input_arr, filepath))
 
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+
