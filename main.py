c0d3558b1da43f634542bda44ec18979585b7b9d
hunglt97 <33819290+hunglt97@users.noreply.github.com>
2020-12-12 14:56:43 +0700

    Update main.py

--- main.py
+++ main.py
@@ -23,27 +23,39 @@ if __name__ == '__main__':
     # model = fbdnet.my_model()
     # fbdnet.train_model(x_train, y_train_beauty, y_train_race, y_train_gender, filepath)
 
+
     y_pred_beauty, y_pred_race, y_pred_gender = fbdnet.predict_model(x_test, filepath)
 
-    cov = np.cov(y_pred_beauty, y_test_beauty)
-    print('pc =', cov/(np.std(y_pred_beauty)*np.std(y_test_beauty)))
-    print('mae =', np.mean(np.abs(y_pred_beauty-y_test_beauty)))
-    print('rmse =', np.sqrt(np.mean(np.square(y_pred_beauty-y_test_beauty))))
-    print('race acc', accuracy_score(y_test_race, y_pred_race))
-    print('gender acc', accuracy_score(y_test_gender, y_pred_gender))
+    # cov = np.cov(y_pred_beauty, y_test_beauty)
+    # print('pc =', cov/(np.std(y_pred_beauty)*np.std(y_test_beauty)))
+    # print('mae =', np.mean(np.abs(y_pred_beauty-y_test_beauty)))
+    # print('rmse =', np.sqrt(np.mean(np.square(y_pred_beauty-y_test_beauty))))
+    # print('race acc', accuracy_score(y_test_race, y_pred_race))
+    # print('gender acc', accuracy_score(y_test_gender, y_pred_gender))
 
-    for i in range(2, 200, 40):
-        test_img = x_test[i]
-        test_img = test_img[np.newaxis, :, :, :]
-        y_pred_beauty_1, y_pred_race_1, y_pred_gender_1 = fbdnet.predict_model(test_img, filepath)
-        print(y_test_beauty[i], y_pred_beauty_1)
-        print(y_test_race[i], y_pred_race_1)
-        print(y_test_gender[i], y_pred_gender_1)
+    # for i in range(2, 200, 40):
+    #     test_img = x_test[i]
+    #     test_img = test_img[np.newaxis, :, :, :]
+    #     y_pred_beauty_1, y_pred_race_1, y_pred_gender_1 = fbdnet.predict_model(test_img, filepath)
+    #     print(y_test_beauty[i], y_pred_beauty_1)
+    #     print(y_test_race[i], y_pred_race_1)
+    #     print(y_test_gender[i], y_pred_gender_1)
 
-    fbdnet.demo('test1.jpg', filepath)
-    fbdnet.demo('test2.jpg', filepath)
-    fbdnet.demo('test3.jpg', filepath)
-    fbdnet.demo('test4.jpg', filepath)
+    # fbdnet.demo('abg2.jpg', filepath)
+    # # fbdnet.demo('phuc0.jpg', filepath)
+    # fbdnet.demo('edward.jpg', filepath)
+    # fbdnet.demo('edward0.jpg', filepath)
+    # fbdnet.demo('tuyen.png', filepath)
+    # fbdnet.demo('trump1.jpg', filepath)
+    # fbdnet.demo('nghia1.jpg', filepath)
+    # fbdnet.demo('trump10.jpg', filepath)
+    fbdnet.demo('1a0.jpg', filepath)
+    fbdnet.demo('1a1.jpg', filepath)
+    fbdnet.demo('1a2.jpg', filepath)
+    fbdnet.demo('1a3.jpg', filepath)
+    fbdnet.demo('1a4.jpg', filepath)
+    # fbdnet.demo('tuyen.png', filepath)
+
 
 
 
